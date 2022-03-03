0b8237f5fb2afb139f25d07057e28caee82a0fca
mebiol <97887903+mebiol@users.noreply.github.com>
2022-03-03 10:32:40 +0700

    Add files via upload

--- rain_regions.py
+++ rain_regions.py
@@ -2,22 +2,24 @@ import requests
 import json
 from datetime import date
 import os
-import codecs
 
-url = 'https://data.tmd.go.th/api/RainRegions/v1/?uid=api&ukey=api12345&format=json'
-secret = {'u65thunchanok1944': ' 65f231a480a39c7e52027023980c35b8'}
+with open('secret.json') as json_file:
+    secrets = json.load(json_file)
+
+uid = secrets['uid']
+ukey = secrets['ukey']
+
+url = f'https://data.tmd.go.th/api/RainRegions/v1/?uid={uid}&ukey={ukey}&format=json'
+
 day = date.today()
 y =  day.year
 m =  day.month
-
-x = requests.get(url, data = secret)
-print(x)
+x = requests.get(url)
 decoded_data = x.text.encode().decode('utf-8-sig')
 data = json.loads(decoded_data)
-print(type(data))
 
-path = f'RainRegions/year={y}/month={m}'
+path = f"RainRegions/year={y}/month={m}"
 os.makedirs(path, exist_ok=True)
 
-with open(f'{path}{day}.json', 'w',encoding = 'utf-8-sig') as f:
+with open(f'{path}/{day}.json', 'w',encoding = 'utf-8-sig') as f:
     json.dump(data, f, indent=4, ensure_ascii=False)
